b2dfbf3ec18636f1242fb4e7c54737f76f481b1c
Mike Wilson <knobby@burntsheep.com>
2019-01-24 11:28:10 -0500

    Adding support for multiple IP addresses. Useful in situations where there isn't a 1-1 mapping of unit to IP such as virtual IP addresses

--- provides.py
+++ provides.py
@@ -1,23 +1,37 @@
-from charms.reactive import hook
-from charms.reactive import RelationBase
-from charms.reactive import scopes
+import json
+
+from charms.reactive import when, when_not
+from charms.reactive import set_flag, clear_flag
+from charms.reactive import Endpoint
 
 
-class PublicAddressProvides(RelationBase):
-    '''This class provides a public address and port to other units.'''
-    scope = scopes.UNIT
+class PublicAdddressProvides(Endpoint):
 
-    @hook('{provides:public-address}-relation-{joined,changed}')
-    def changed(self):
-        self.set_state('{relation_name}.available')
+    @when('endpoint.{endpoint_name}.joined')
+    def joined(self):
+        set_flag(self.expand_name('{endpoint_name}.available'))
 
-    @hook('{provides:public-address}-relation-{broken,departed}')
+    @when_not('endpoint.{endpoint_name}.joined')
     def broken(self):
-        self.remove_state('{relation_name}.available')
+        clear_flag(self.expand_name('{endpoint_name}.available'))
 
     def set_address_port(self, address, port):
         # Iterate over all conversations of this type to send data to everyone.
-        for conversation in self.conversations():
-            client = {'public-address': address, 'port': port}
-            # Send the address and port to every unit using the conversation.
-            conversation.set_remote(data=client)
+        if type(address) is list:
+            # build 2 lists to zip together that are the same length
+            length = len(address)
+            p = [port] * length
+            combined = zip(address, p)
+            clients = [{'public-address': a, 'port': p}
+                       for a, p in combined]
+            # for backwards compatibility, we just send a single entry
+            # and have an array of dictionaries in a field of that
+            # entry for the other entries.
+            first = clients.pop(0)
+            first['extended_data'] = json.dumps(clients)
+            for relation in self.relations:
+                relation.to_publish_raw.update(first)
+        else:
+            for relation in self.relations:
+                relation.to_publish_raw.update({'public-address': address,
+                                                'port': port})
